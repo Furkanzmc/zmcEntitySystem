0d0b51fc4dbb8b04cbebfe9ba11f01957a910663
Furkanzmc <furkanuzumcu@gmail.com>
2014-03-27 22:24:56 +0200

    Remove unnecessary virtual functons and change std::shared_ptr to pointer

--- BaseEntity.h
+++ BaseEntity.h
@@ -21,43 +21,43 @@ public:
      * @brief Returns true if the entity is enabled. Enabled entities can be processed by the EntitySystem
      * @return
      */
-    virtual bool isEnabled();
+    bool isEnabled();
     /**
      * @brief Returns true if the entity is disabled. Disabled entities can't be processed by the EntitySystem
      * @return
      */
-    virtual bool isDisabled();
+    bool isDisabled();
     /**
      * @brief Set to true to enable the entity, or false to disable it
      * @param enable
      * @return
      */
-    virtual void setEnabled(bool enable);
+    void setEnabled(bool enable);
     /**
      * @brief Set the group of the entity. -1 value means the entity has no group
      * @param groupIdentifier
      */
-    virtual void setGroup(int groupIdentifier);
+    void setGroup(int groupIdentifier);
     /**
      * @brief Returns a non-zero value if the entity has a group otherwise returns -1
      * @return
      */
-    virtual int getGroup();
+    int getGroup();
 
-    virtual void setEntityID(int id);
-    virtual int getEntityID() const;
-    virtual void setComponentManager(std::shared_ptr<ComponentManager> componentManager);
-    virtual std::shared_ptr<ComponentManager> getComponentManager();
-    virtual bool hasComponent(int componentType);
+    void setEntityID(int id);
+    int getEntityID() const;
+    void setComponentManager(ComponentManager *componentManager);
+    ComponentManager *getComponentManager();
+    bool hasComponent(int componentType);
 
-protected:
+private:
     bool mIsEnabled;
     int mEntityID;
     /**
      * @brief -1 means the entity has no group
      */
     int mEntityGroup;
-    std::shared_ptr<ComponentManager> mComponentManager;
+    ComponentManager *mComponentManager;
 };
 }
 #endif // BASEENTITY_H
